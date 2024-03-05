0270a5d4c6a3f4e85facd92c847a1f6c70542b6f
DaddyDany <105244858+DaddyDany@users.noreply.github.com>
2024-03-04 19:57:09 -0600

    examen

---
+++ FrontLibros/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: import.meta.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect: "/books"
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: "/books",
+      name: "books",
+      component: () => import("../views/Books.vue")
+    },
+  ]
+})
+
+export default router
